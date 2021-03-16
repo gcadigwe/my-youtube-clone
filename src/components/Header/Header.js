6860fcecfc6a1a01beec627ec58a1ca670fd7fdf
GCAdigwe <gcadigwe@gmail.com>
2021-03-16 14:47:14 +0000

    done with header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+import MicIcon from "@material-ui/icons/Mic";
+
+function Header() {
+  const [value, setValue] = useState("");
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1200px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__input">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+        <SearchIcon className="header__inputButton" />
+      </div>
+      <div className="header__right">
+        <VideoCallSharpIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/smartest-dog-breeds-1553287693.jpg?crop=0.673xw:1.00xh;0.167xw,0&resize=640:*" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5DG8sJGnbaZRaYccX7iM_y_Ho-bGN6S4",
+  authDomain: "clone-d00af.firebaseapp.com",
+  projectId: "clone-d00af",
+  storageBucket: "clone-d00af.appspot.com",
+  messagingSenderId: "698663768524",
+  appId: "1:698663768524:web:fd20652f99d8cf3f6a4d69",
+  measurementId: "G-5PQ66TS8R9",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
